dbaea679303df81e2b80cb09e1444057da33f31a
hyun471 <khs101ttl@gmail.com>
2025-07-31 09:52:56 +0900

    0729해답 추가

--- lib/lib/7/answer/ui_0729.dart
+++ lib/lib/7/answer/ui_0729.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 
+// 달력 출력
+// DateTime.now()
 class Home0729Answer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    DateTime date = DateTime(2025, 07, 31);
+    // 어떤 날짜를 입력받더라도 해당 날짜가 속해있는 달의 달력 출력!
+    // DateTime date = DateTime.now();
+    // DateTime date = DateTime(2025, 02, 02);
+    DateTime date = DateTime(2024, 02, 15);
     DateTime beginDate = DateTime(date.year, date.month, 1);
     final endOfmonths = [
       31,
@@ -19,28 +24,92 @@ class Home0729Answer extends StatelessWidget {
       30,
       31,
     ];
-    int weekday = beginDate.weekday == 7 ? 0 : beginDate.weekday;
     int endDate = endOfmonths[date.month - 1];
+    if (date.month == 2 && date.year % 4 == 0) {
+      endDate = 29;
+    }
+    int weekday = beginDate.weekday == 7 ? 0 : beginDate.weekday;
+
     List<String> days = [];
     for (var i = 0; i < weekday; i++) {
       days.add('');
     }
+    for (var i = 1; i <= endDate; i++) {
+      days.add("$i");
+    }
+    while (days.length % 7 != 0) {
+      days.add('');
+    }
+
+    int weekCount = (days.length / 7).ceil();
+
     return Scaffold(
       appBar: AppBar(title: Text('달력')),
       body: Column(
         children: [
           // 1. Row 이용해서 구현
           labelRow(),
-          week(['', '', '1', '2', '3', '4', '5']),
+          week([
+            days[0],
+            days[1],
+            days[2],
+            days[3],
+            days[4],
+            days[5],
+            days[6],
+          ]),
           SizedBox(height: 4),
-          week(['6', '7', '8', '9', '10', '11', '12']),
+          week([
+            days[7],
+            days[8],
+            days[9],
+            days[10],
+            days[11],
+            days[12],
+            days[13],
+          ]),
           SizedBox(height: 4),
-          week(['13', '14', '15', '16', '17', '18', '19']),
+          week([
+            days[14],
+            days[15],
+            days[16],
+            days[17],
+            days[18],
+            days[19],
+            days[20],
+          ]),
           SizedBox(height: 4),
-          week(['20', '21', '22', '23', '24', '25', '26']),
+          week([
+            days[21],
+            days[22],
+            days[23],
+            days[24],
+            days[25],
+            days[26],
+            days[27],
+          ]),
           SizedBox(height: 4),
-          week(['27', '28', '29', '30', '31', '', '']),
+          if (weekCount > 4)
+            week([
+              days[28],
+              days[29],
+              days[30],
+              days[31],
+              days[32],
+              days[33],
+              days[34],
+            ]),
           // 2. GridView 이용해서 구현
+          if (weekCount > 5)
+            week([
+              days[35],
+              days[36],
+              days[37],
+              days[38],
+              days[39],
+              days[40],
+              days[41],
+            ]),
           grid(),
         ],
       ),
@@ -48,10 +117,37 @@ class Home0729Answer extends StatelessWidget {
   }
 
   Widget grid() {
-    List<Widget> days = List.generate(33, (index) {
-      final value = index - 1;
-      return gridItem(value <= 0 ? "" : "$value");
-    });
+    DateTime date = DateTime.now();
+
+    DateTime beginDate = DateTime(date.year, date.month, 1);
+    final endOfmonths = [
+      31,
+      28,
+      31,
+      30,
+      31,
+      30,
+      31,
+      31,
+      30,
+      31,
+      30,
+      31,
+    ];
+    int endDate = endOfmonths[date.month - 1];
+    if (date.month == 2 && date.year % 4 == 0) {
+      endDate = 29;
+    }
+    int weekday = beginDate.weekday == 7 ? 0 : beginDate.weekday;
+
+    List<Widget> days = [];
+    for (var i = 0; i < weekday; i++) {
+      days.add(gridItem(''));
+    }
+    for (var i = 1; i <= endDate; i++) {
+      days.add(gridItem("$i"));
+    }
+
     return Container(
       height: 300,
       color: Colors.blue,
